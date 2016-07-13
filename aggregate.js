8e17cce7613490db0a756a450c82428d7c262433
Nishtha <nishu.gakkhar30@gmail.com>
2016-07-13 12:08:13 +0530

    final files created

--- aggregate.js
+++ aggregate.js
@@ -6,8 +6,6 @@ var arrayOne= stringData.split('\r\n');
 var noOfRow=arrayOne.length;
 var final_obj={};
 var header=arrayOne[0].split(',');
-var pop='Population (Millions) 2013';
-var gdp='GDP Billions (USD) 2013';
 var jArray=[];
 var newi=header.indexOf('Country Name');
 var cnt=header.indexOf('Population (Millions) 2013');
@@ -15,13 +13,9 @@ var cnt1=header.indexOf('GDP Billions (USD) 2013');
   for (i = 1; i < noOfRow-1; i++) {
     var line=arrayOne[i].split(',');
     if(line[newi] != "European Union") {
-      if ( final_obj[map[line[newi]]]==undefined) {
+      if ( final_obj[map[line[newi]]]==undefined || final_obj[map[line[newi]]][header[cnt]]==undefined || final_obj[map[line[newi]]][header[cnt1]]==undefined) {
         final_obj[map[line[newi]]]={};
-      }
-      if(final_obj[map[line[newi]]][header[cnt]]==undefined) {
         final_obj[map[line[newi]]][header[cnt]] = 0;
-      }
-      if(final_obj[map[line[newi]]][header[cnt1]]==undefined) {
         final_obj[map[line[newi]]][header[cnt1]] = 0;
       }
       final_obj[map[line[newi]]][header[cnt]]+=parseFloat(line[cnt]);
